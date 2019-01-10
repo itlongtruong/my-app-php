c8f7eff1093c13ffcdf2c7c17fe7a1b9769461bb
baohoangfc <baohoangfc@gmail.com>
2019-01-10 15:39:22 +0700

    next

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -87,7 +87,7 @@
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">New</a>
+                    <a href="https://laravel-news.com">Newssssssssssssssssssssssssssssssss</a>
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
